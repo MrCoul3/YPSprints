169895ab3bd76f116b09e9fce892ef73fb6bf7f5
MrCoul3 <53871298+MrCoul3@users.noreply.github.com>
2022-05-25 21:35:18 +0300

    sprint_1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-/*
-	* range(4); // => [0, 1, 2, 3] 
-	* range(-4); // => [0, -1, -2, -3]
-	* range(1, 5); // => [1, 2, 3, 4]
-	* range(0, 20, 5); // => [0, 5, 10, 15]
-	* range(0, -4, -1); // => [0, -1, -2, -3]
-	* range(1, 4, 0); // => [1, 1, 1]
-	* range(0); // => []
-*/
-
-function range(start, end, step) {
-    if (!step) {
-
-    }
-}
-
-console.log(range(1, 5))
+console.log('index')
+const btnLogin = document.querySelector('.btn__login');
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    location.href = '/chat.html'
+})
