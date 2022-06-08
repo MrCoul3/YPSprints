bc6cbe5fe3321eadd3ae54c0415183bc3c0fab64
MrCoul3 <53871298+MrCoul3@users.noreply.github.com>
2022-06-08 21:01:19 +0300

    sprint 2 работа с компонентами

---
+++ src/pages/login/login.ts
@@ -0,0 +1,13 @@
+import Block from 'core/Block';
+
+interface IProps {}
+
+export class Login extends Block {
+    constructor(props: IProps) {
+        super({...props});
+    }
+
+    render(): string {
+        return super.render();
+    }
+}
